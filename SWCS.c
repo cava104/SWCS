8ec0c58c638ef21961385c308b583f65c8e8afb1
cava104 <128607890+cava104@users.noreply.github.com>
2023-11-09 11:19:56 +0100

    vPre-Alpha 0.002

--- SWCS.c
+++ SWCS.c
@@ -1,26 +1,47 @@
 #include <stdio.h>
-//Star Wars: Cantina Simulator V0.001
+#include <stdlib.h>
+#include <windows.h>
+//Star Wars: Cantina Simulator V0.002
 void menu(){
 	char n;
 	printf("Star Wars: Cantina Simulator\n\n");
 	printf("1) Nuova Partita\n");
 	printf("2) Carica Partita\n\n");
-	printf("v0.001\n");
+	printf("v0.002\n");
 	do{
 		scanf("%c", &n);	
 	}while(n < '1' || n > '2');
 	
 	switch(n){
 		case '1':
+			PlaySound(TEXT("ButtonPressed.wav"),NULL,SND_ASYNC | SND_NOSTOP);
 			system("cls");
-			printf("Nuova\n");
+			printf("Nuova Partita\n\n");
+			printf("1) Slot 1 | VUOTO |\n");
+			printf("2) Slot 2 | VUOTO |\n");
+			printf("3) Slot 3 | VUOTO |\n");
+			printf("4) Indietro\n");
+			do{
+				scanf("%c", &n);	
+			}while(n < '4' || n > '4');
+			switch(n){
+				case '4':
+					PlaySound(TEXT("ButtonPressed.wav"),NULL,SND_ASYNC | SND_NOSTOP);
+					system("cls");
+					menu();
+					break;
+			}
+			
 			break;
 		case '2':
+			PlaySound(TEXT("ButtonPressed.wav"),NULL,SND_ASYNC);
 			system("cls");
 			printf("Carica\n");
+			sleep(10);
 			break;
 	}
 }
 int main(){
+	PlaySound(TEXT("MenuTheme.wav"),NULL,SND_ASYNC | SND_NOSTOP);
 	menu();
 }
