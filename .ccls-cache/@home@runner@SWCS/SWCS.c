e034861bfca20798969102b4bda529d76cf2ea8a
Replit user <>
2023-12-05 11:43:54 +0000

    a

---
+++ .ccls-cache/@home@runner@SWCS/SWCS.c
@@ -0,0 +1,9 @@
+#include "src/data.c"
+//Star Wars: Cantina Simulator V0.002
+
+int main(){
+    struct Player player;
+    InitPlayer(&player);
+
+    MainMenu(&player);
+}
---
+++ .ccls-cache/@home@runner@SWCS/src@data.c
@@ -0,0 +1,74 @@
+#include "funzioni.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <conio.h>
+#include <stdbool.h>
+    
+#define DataPath "data/"
+
+char IntToChar(int val){
+   return char ch = '0' + val; 
+}
+
+int SaveData(int currentSlot,struct Bar* bar){
+    char* str  = malloc(2);
+    sprintf(str,"%d",currentSlot);
+    char* SavePath = (char*)malloc(strlen(DataPath)+strlen(str)+4);
+    strcpy(SavePath,DataPath);
+    strcat(SavePath,"Save-");
+    strcat(SavePath,IntToChar(currentSlot);
+    strcat(SavePath,".txt");
+    FILE* Save = fopen(SavePath,"wb");
+
+    if(!Save){
+        printf("Couldn't Save:\n");
+        free(SavePath);
+        return -1;
+    }
+    fwrite(bar,sizeof(struct Bar),1,Save);
+    fclose(Save);
+    free(SavePath);
+    return 1;
+}
+
+int LoadData(int Slot,struct Bar* bar){
+    char* str = malloc(2);
+    sprintf(str,"%d",Slot);
+    char* LoadPath = (char*)malloc(strlen(DataPath)+strlen(str)+4);
+    strcpy(SavePath,DataPath);
+    strcat(SavePath,"Save-");
+    strcat(SavePath,IntToChar(currentSlot);
+    strcat(SavePath,".txt");
+    FILE* Load = fopen(LoadPath,"rb");
+
+    if(!Load){
+        perror("");
+        free(LoadPath);
+        return -1;
+    }
+
+    fread(bar, sizeof(struct Bar), 1, Load);
+
+    fclose(Load);
+    free(LoadPath);
+    return 1;
+}
+
+bool FileExist(int Slot){
+    char* str = malloc(2);
+    sprintf(str,"%d",Slot);
+    char* findFile = (char*)malloc(strlen(DataPath)+strlen(str)+4);
+    strcpy(SavePath,DataPath);
+    strcat(SavePath,"Save-");
+    strcat(SavePath,IntToChar(currentSlot);
+    strcat(SavePath,".txt");
+
+    FILE* file = fopen(findFile,"r");
+
+    if(file == NULL){
+        return false;
+    }
+    free(findFile);
+    return true;
+}
